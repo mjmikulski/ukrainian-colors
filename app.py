5eff40f3315030add9d05b177b120a90ddb80e0d
Maciej Mikulski <maciej.mikulski@netguru.com>
2022-03-04 16:04:42 +0100

    Add streamlit app

---
+++ app.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import streamlit as st
+
+from ukraine import make_ukrainian_colors
+
+st.title('Ukraine colors on your avatar')
+
+POSSIBLE_EXTENSIONS = ('png', 'jpg', 'jpeg')
+
+input_file = st.file_uploader('Upload your image', type=POSSIBLE_EXTENSIONS)
+
+if input_file is not None:
+    type_, extension = input_file.type.lower().split('/')
+
+    if type_ != 'image':
+        st.error('Wrong input_file type.')
+        st.stop()
+
+    if extension not in POSSIBLE_EXTENSIONS:
+        st.error(f'Unknown extension of the uploaded input_file: {extension}')
+        st.stop()
+
+    output_filename = f'Go_Ukraine_{input_file.name}'
+
+    file_bytes = np.asarray(bytearray(input_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+
+    img_ua = make_ukrainian_colors(img, saturate=True)
+
+    temp_file = f'temp.{extension}'
+    cv2.imwrite(temp_file, img_ua)
+
+    st.image(img_ua, channels='bgr')
+
+    with open(temp_file, 'rb') as output_file:
+        st.download_button('Download', output_file, file_name=output_filename, mime=input_file.type)
